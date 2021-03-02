cb56345d6db0c979f89c1f1b556e437cbefa6c4d
Simon Solders <simon.solders@gmail.com>
2021-03-02 17:15:46 +0100

    New project from create-react-app and typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import _PaymentIQCashier, { ApiMethodsType, CashierConfig } from 'paymentiq-cashier-bootstrapper'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  componentDidMount () {
+    const config: CashierConfig = {
+      merchantId: '1337',
+      userId: '123',
+      sessionId: '66',
+      environment: 'test', // if not set, defaults to production
+      method: 'deposit' // if not set, defaults to deposit
+    }
+
+    new _PaymentIQCashier('#cashier', config,
+      (api: ApiMethodsType) => {
+        console.log('Cashier intialized and ready to take down the empire', api)
+        console.log(api)
+        api.css(`
+          html { color: pink; }
+        `)
+        api.on({
+          success: (data: any) => { console.log(data) }
+        })
+      }
+    )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello world</h1>
+        </header>
+  
+        <div id='cashier'></div>
+      </div>
+    );
+  }
 }
 
 export default App;
